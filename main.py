7d0ccb6e92d27344a83ec8ec2dc8c59073b5e920
Max <Max2827@yande.ru>
2022-02-28 19:25:21 +0300

    create my own page

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Hello, world!")
-name = input('Enter your name: ')
-print('Hello,', name)
+from fastapi import FastAPI, Body
+import uvicorn
+from fastapi.responses import PlainTextResponse
+
+
+app = FastAPI()
+
+@app.get('/')
+def start_window():
+    s = 'Чё-то на странице'
+    return PlainTextResponse(s)
+
+
+uvicorn.run(app)
